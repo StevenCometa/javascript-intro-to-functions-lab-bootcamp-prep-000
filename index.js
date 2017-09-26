b373f4d944c9bdebb34355404f261411a4fff280
Steven Cometa <sdcometa@gmail.com>
2017-09-26 06:40:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+var uppercase = "Hello"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = `hello`
+
+lowercase.toLowerCase() === lowercase
+
+function sayHitoGrandma(string) {
+  if(string === uppercase) 
+  return "YES INDEED!"
+} else {if (string === lowercase)
+  return "I can't hear you!"
+} else {if (string === "I love you, Grandma.")
+  return "I love you, too."
+}
